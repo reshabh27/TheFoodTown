425b216d5ccec5fcfa9eb672bbf9a1885a1a445c
Reshabh <reshabhsahu10@gmail.com>
2022-08-11 22:36:21 +0530

    files updated

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipeItem = props => {
+  return (
+      <div class="card py-2 text-center">
+        
+      </div>
+  );
+};
+
+export default RecipeItem;
---
+++ src/components/Recipes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipes = props => {
+
+  return (
+    <div class="card-columns">
+     
+    </div>
+  );
+};
+
+export default Recipes;
